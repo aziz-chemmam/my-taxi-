681792523dd651aa876f22f76079dc4d0828d142
aziz chemmam <azizchemmam123@gmail.com>
2024-02-09 17:03:02 +0100

    first commit

---
+++ database/migrations/2024_02_09_154444_create-reservation-table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+    
+            Schema::create('reservations', function (Blueprint $table) {
+                $table->id();
+                $table->unsignedBigInteger('driver_id');
+                $table->unsignedBigInteger('passenger_id');
+                $table->string('status');
+                $table->unsignedBigInteger('route_id');
+                $table->foreign('driver_id')->references('id')->on('drivers')->cascadeOnDelete()->cascadeOnUpdate();
+                $table->foreign('passenger_id')->references('id')->on('passengers')->cascadeOnDelete()->cascadeOnUpdate();
+                $table->foreign('route_id')->references('id')->on('routes')->cascadeOnDelete()->cascadeOnUpdate();
+                $table->timestamps();
+            });
+        }
+    
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
